292d842544c32867f1783906c6fc09f52bac2eba
Luca Martinelli <lucxmangajet@gmail.com>
2021-03-20 17:47:31 +0100

    added new player controller subtypes

---
+++ ShieldHero/Source/ShieldHero/ProjectileActor.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "ProjectileActor.generated.h"
+
+UCLASS()
+class SHIELDHERO_API AProjectileActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	AProjectileActor();
+
+};
