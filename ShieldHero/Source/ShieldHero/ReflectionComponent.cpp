7bbcad844b8a094d1d1ef8a44c6e52648fea6807
Luca Martinelli <lucxmangajet@gmail.com>
2021-03-25 00:39:08 +0100

    moved custom arrow bouncing to reflection component

--- ShieldHero/Source/ShieldHero/ReflectionComponent.cpp
+++ ShieldHero/Source/ShieldHero/ReflectionComponent.cpp
@@ -1,14 +1,57 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "ReflectionComponent.h"
+#include "ShieldHeroGameMode.h"
+#include "GenericPlatform/GenericPlatformMath.h"
 
 // Sets default values for this component's properties
 UReflectionComponent::UReflectionComponent()
 {
-	
+
 
 }
 
+void UReflectionComponent::NotifyCustomReflection(AActor* actor, UProjectileMovementComponent* projectileMovement, FVector hitNormal)
+{
+	OnCustomReflection.Broadcast(actor, projectileMovement, hitNormal);
+}
+
+AActor* UReflectionComponent::FindBestEnemyInDirectionAfterReflection(FVector origin, FVector normal, FVector inVelocity, float maxDistance)
+{
+	AShieldHeroGameMode* gameMode = GetWorld()->GetAuthGameMode<AShieldHeroGameMode>();
+
+	TArray<AActor*>* enemies = gameMode->GetEnemies();
+
+	float maxDot = TNumericLimits<float>::Min();
+	AActor* bestEnemy = nullptr;
+
+	for (size_t i = 0; i < enemies->Num(); i++)
+	{
+		AActor* enemy = (*enemies)[i];
 
 
+		FVector toEnemy = enemy->GetActorLocation() - origin;
+
+		//InRange
+		if (toEnemy.Size() < maxDistance)
+		{
+			FVector normalizedToEnemy = toEnemy.GetSafeNormal();
+
+			//InDirection
+			if (FVector::DotProduct(normalizedToEnemy, normal) > 0)
+			{
+
+				FVector reflectedVelocity = FMath::GetReflectionVector(inVelocity, normal);
+
+				float reflectedDot = FVector::DotProduct(reflectedVelocity, normalizedToEnemy);
+				if (reflectedDot > maxDot)
+				{
+					maxDot = reflectedDot;
+					bestEnemy = enemy;
+				}
+			}
+		}
+	}
+
+	return bestEnemy;
+}
--- ShieldHero/Source/ShieldHero/ReflectionComponent.h
+++ ShieldHero/Source/ShieldHero/ReflectionComponent.h
@@ -6,25 +6,39 @@
 #include "Components/ActorComponent.h"
 #include "ReflectionComponent.generated.h"
 
+class UProjectileMovementComponent;
+
 UENUM(BlueprintType)
 enum class ReflectionType : uint8
 {
 	Reflect,
 	StopAndDamage,
 	ReflectAndDamage,
-	ReflectDirected
+	Custom
 };
 
-UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_ThreeParams(FCustomReflectionDelegate, AActor*, actor, UProjectileMovementComponent*, projectileMovement, FVector, hitNormal);
+
+
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
 class SHIELDHERO_API UReflectionComponent : public USceneComponent
 {
 	GENERATED_BODY()
 
-public:	
+public:
 	// Sets default values for this component's properties
 	UReflectionComponent();
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Gameplay)
-		ReflectionType _reflectionType;
+	ReflectionType _reflectionType;
+
+	UFUNCTION(BlueprintCallable)
+	void NotifyCustomReflection(AActor* actor, UProjectileMovementComponent* projectileMovement, FVector hitNormal);
+
+	UFUNCTION(BlueprintCallable, Category = "Gameplay")
+	AActor* FindBestEnemyInDirectionAfterReflection(FVector origin, FVector normal, FVector inVelocity, float maxDistance);
+
+	UPROPERTY(BlueprintAssignable, Category = "Gameplay")
+	FCustomReflectionDelegate OnCustomReflection;
 
 };
